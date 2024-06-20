448719ba8d0172fa51355a6e31b760131ce94e0e
rfpacheco <ronnyfph@gmail.com>
2024-06-20 12:30:34 +0200

    Finishe the first and main part of subfamilies classification

---
+++ 21.Subfamilies_classification/1.subfamilies_classifier.py
@@ -0,0 +1,104 @@
+# Needed modules
+import argparse
+import pandas as pd
+import subprocess
+import os
+import re
+
+from Bio import Seq
+from Bio.SeqRecord import SeqRecord
+
+from subfamilies_global_functions import blastn_dic, blastn_blaster, fasta_creator, save_sequences_to_csv_pandas, find_maximal_sets, count_sequences, join_conflicted_sequences, subfamily_naming
+
+# =============================================================================
+# Defining arparse arguments
+# =============================================================================
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Classify SIDER elements in subfamilies.')
+    parser.add_argument('-f', '--file', type=str, required=True, help='Path to the CSV file with the positive SIDER elements.')
+    parser.add_argument('-o', '--output', type=str, required=True, help='Path to the output directory to save the data.')
+    parser.add_argument('-n', '--naming', type=str, required=True, help='Naming of the subfamilies.')
+    return parser.parse_args()
+
+# =============================================================================
+# Defining needed functions
+# =============================================================================
+def grouping_subfamily(input_df, path_output, naming):
+    # Group <input_df> by chromosome
+    repeated_chr = []  # List to store the repeated chromosomes
+    input_df_grouped = input_df.groupby('sseqid')  # Data type: pandas groupby object
+    for index, group in input_df_grouped:  # group is a pandas dataframe
+        # Set working folder
+        path_chr_folder = os.path.join(path_output, index)   # Folder for each chr, i.e., index
+        os.makedirs(path_chr_folder, exist_ok=True)  # Create the folder
+        print(f'Chromosome {index} folder created at {path_chr_folder}')
+
+        # Create the fasta file
+        path_fasta_file = os.path.join(path_chr_folder, f'{index}.fasta')
+        fasta_creator(data=group, fasta_output_path=path_fasta_file)
+
+        # Create the BLAST database
+        path_dict_folder = os.path.join(path_chr_folder, 'blast_dict')
+        os.makedirs(path_dict_folder, exist_ok=True)
+        path_dict_file = os.path.join(path_dict_folder, os.path.basename(path_fasta_file))
+        blastn_dic(path_input=path_fasta_file, path_output=path_dict_file)
+
+        # BLASTN
+        data = blastn_blaster(query=path_fasta_file, path_genome=path_dict_file, identity=85)  # in each chr launch the BLASTn sequence against each other.
+
+        # Skip if the dataframe is empty
+        if data.empty:
+            continue  # If the dataframe is empty, continue with the next iteration
+        else:
+            pass
+
+        # Filter by length
+        data = data[data['length'] > 100].copy()  # Filter by length
+
+        # Create a dictionary with the sequences
+        main_dict = {}
+        for query in data['qseqid'].unique():
+            values = data[data['qseqid'] == query].loc[:, ['sseqid']].values.flatten().tolist()  # For each query, get the values from 'sseqid' that match with that query.
+            values = list(set(values))  # Remove duplicates
+            values = sorted(values)
+            main_dict[query] = values
+        main_dict = {key: sorted(value, key=lambda x: int(re.findall(r'_(\d+)_', x)[0])) for key, value in main_dict.items()}  # Inside each key in the dicctionary, sort the values by the number of the sequence
+
+        unique_values = []
+        [unique_values.append(value) for _, value in main_dict.items() if value not in unique_values]
+        # Get the unique values from the dictionary
+        unique_values = find_maximal_sets(sequences=unique_values)  # Find the maximal sets. # Find the maximal sets. If there's a list [1, 2] and a list [1, 2, 3], it will remove the first one.
+        unique_values = join_conflicted_sequences(sequences=unique_values)  # Join the conflicted sequences. # Join the conflicted sequences. If there's a list [1, 2] and a list [2, 3], it will join them in a list [1, 2, 3]
+        named_elements = subfamily_naming(chromosome=index, naming=naming, sequences=unique_values)
+        counters = count_sequences(sequences=unique_values)
+
+        if len(counters) > 0:
+            repeated_chr.append(index)
+            unique_values.append("Repeated values:")
+            combined_list = [item for pair in zip(counters.keys(), counters.values()) for item in pair]
+            unique_values.append(combined_list)  # Append the sequences that appear more than once
+
+        save_sequences_to_csv_pandas(data=unique_values, filename=os.path.join(path_chr_folder, f'families_{index}.csv'))
+        save_sequences_to_csv_pandas(data=named_elements, filename=os.path.join(path_chr_folder, f'NAMED_families_{index}.csv'))
+
+    print(f'{len(repeated_chr)} chromosomes with repeated values: {repeated_chr}')
+
+        
+# =============================================================================
+# Main function
+# =============================================================================
+if __name__ == '__main__':
+    args = parse_arguments()  # Parse arguments
+
+    # Read data needed
+    data = pd.read_csv(args.file, sep=',', header=0)
+
+    # Create the output folder
+    path_folder = os.path.join(args.output, 'subfamilies_classification')
+    os.makedirs(path_folder, exist_ok=True)
+    print(f'Principal folder created in {path_folder}')
+
+    # call the function
+    grouping_subfamily(input_df=data, path_output=path_folder, naming=args.naming)
+
+
---
+++ 21.Subfamilies_classification/subfamilies_global_functions.py
@@ -0,0 +1,158 @@
+# R. Pacheco #
+# =============================================================================
+# Importing needed modules
+# =============================================================================
+import pandas as pd
+import subprocess
+import os
+import re
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+# =============================================================================
+# Defining needed functions
+# =============================================================================
+def blastn_dic(path_input, path_output):
+    cmd = f'makeblastdb -in {path_input} -dbtype nucl -parse_seqids -out {path_output}'
+    subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+# -----------------------------------------------------------------------------
+def blastn_blaster(query, path_genome, identity):
+    cmd = (
+        f'blastn -word_size 11 '
+        f'-query {query} '
+        f'-db {path_genome} '
+        f'-perc_identity {identity} '
+        f'-outfmt 10'
+    )
+    data = subprocess.run(cmd, shell=True, capture_output=True, text=True, universal_newlines=True, executable='/usr/bin/bash')
+    data = data.stdout
+    data_df = pd.DataFrame(
+        [x.split(',') for x in data.split('\n') if x],
+        columns=['qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore'] 
+    )
+    if not data_df.empty:
+        data_df[['pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore']] = data_df[['pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore']].apply(pd.to_numeric)  # Convert to numeric
+    else:  # If empty, return an empty dataframe
+        return pd.DataFrame()
+
+    return data_df  # I only need to return the data, no need to transform it into a pandas dataframe since I want to check the number of lines with the line separator '\n'
+
+
+# -----------------------------------------------------------------------------
+# Get fasta f# Define fasta_creator file from the first chromosome
+def fasta_creator(data, fasta_output_path):
+    matrix = []
+    for index, sequence in data.iterrows():
+        rec = SeqRecord(Seq(sequence['sseq']),
+                        id=f"Seq_{index}_{sequence['sseqid']}",
+                        description="Leishmania infantum"
+                        )
+        matrix.append(rec)
+    SeqIO.write(matrix, fasta_output_path, "fasta")
+
+# -----------------------------------------------------------------------------
+def save_sequences_to_csv_pandas(data, filename):
+    # Convert the list of lists to a DataFrame
+    df = pd.DataFrame(data)
+    # Save the DataFrame to a CSV file
+    df.to_csv(filename, header=False, index=False)
+
+# -----------------------------------------------------------------------------
+def find_maximal_sets(sequences):
+    # Identify maximal sets that are not subsets of any other set in the list
+    # If there are repeated lists. And those lists are the maximun sets, it will break.
+    maximal_sets = []
+    for i, seq in enumerate(sequences):
+        is_subset = False
+        for j, other_seq in enumerate(sequences):
+            # print(f"For index {i} and {j}, sequences are {seq} and {other_seq}")
+            if i != j and set(seq).issubset(set(other_seq)):
+                # print("\tChecking if it's a subset")
+                is_subset = True
+                break
+        if not is_subset:
+            # print(f"\tAdding {seq} to maximal_sets")
+            maximal_sets.append(seq)
+    return maximal_sets
+
+# -----------------------------------------------------------------------------
+def join_conflicted_sequences(sequences):
+    filtered_data = []  # Save the data after all the filtering in this code
+    joines_rel = {}  # Save the index relations that we have already joined.
+    for i, seq1 in enumerate(sequences):
+        counter_no_coincidence = 0  # When seq1 loops through every seq2 and there is no coincidence, we will increase the counter by 1. If the counter is equal to the length of the sequences, we will append the seq1 to the filtered_data, since there is no coincidence with any other seq2.
+
+        for j, seq2 in enumerate(sequences):
+            if i == j:  # Skip the same list
+                counter_no_coincidence += 1
+                continue
+            
+            # Number of coincidence of each element in seq1 with seq2
+            count_coincidences = sum(1 for x in seq1 if x in seq2)
+
+            if count_coincidences >= 1:
+                # Since the two seqs have relations, we will join them
+                new_seq = list(set(seq1 + seq2))  # Join the two lists and remove the repeated elements
+                new_seq = sorted(new_seq)  # Sort the new list
+                filtered_data.append(new_seq)  # Append the new list to the filtered_data
+                
+                # Foolproof for the next iterations
+                seq1 = new_seq  # Update the values for the comparison with the next seq2 in the loop
+                sequences[j] = new_seq  # Update the value for the next iteration when seq2 is called again the the "j" index. It has to be changed in the original sequences list.
+
+                # Save the relations
+                ## Save which elements did "j/seq2" matched with "i/seq1"
+                if j in joines_rel.keys():
+                    joines_rel[j].append(i)
+                else:
+                    joines_rel[j] = [i]
+            else:  # If there is no coincidence, we will increase the counter by 1
+                counter_no_coincidence += 1
+        if counter_no_coincidence == len(sequences):
+            filtered_data.append(seq1)
+    
+    # Remove duplicates lists inside the lists of lists
+    filtered_data_NoDup = []
+    [filtered_data_NoDup.append(value) for value in filtered_data if value not in filtered_data_NoDup]
+
+    data = find_maximal_sets(filtered_data_NoDup)
+    return data
+
+# -----------------------------------------------------------------------------
+def count_sequences(sequences):
+    from collections import Counter
+    # Flatten the list of sequences
+    flat_list = [seq for sublist in sequences for seq in sublist]
+    # Count each sequence using Counter
+    sequence_counts = Counter(flat_list)
+    # Identify sequences that appear more than once
+    multiple_occurrences = {seq: count for seq, count in sequence_counts.items() if count > 1}
+    return multiple_occurrences
+
+# -----------------------------------------------------------------------------
+def subfamily_naming(chromosome, naming, sequences):
+    named_elements = []
+    orphan_counter_id = 10
+    subfamily_abc_counter_id = "A"
+    for index, seqs in enumerate(sequences):  # selecting list
+        if len(seqs) > 1:
+            subfamily = []
+            subfamily_counter_member_id = 10
+            for index2, element in enumerate(seqs):  # selecting element inside a list.
+                chr_id = re.search(r"\d+", chromosome).group(0)
+                subfamily.append(f"{naming}_c{chr_id}.{subfamily_counter_member_id}{subfamily_abc_counter_id}")
+                subfamily_counter_member_id += 10
+            subfamily_abc_counter_id = chr(ord(subfamily_abc_counter_id) + 1)
+
+
+            named_elements.append(subfamily)
+        else: # if len(seqs) == 1
+            chr_id = re.search(r"\d+", chromosome).group(0)
+            named_elements.append([f"{naming}_c{chr_id}.{orphan_counter_id}"])
+            orphan_counter_id += 10
+
+    return named_elements
+
